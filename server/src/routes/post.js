705c88ad3863861802ef6f1d360d851196e3f827
AliMusa04 <tu75nx176@code.edu.az>
2023-02-08 02:21:10 +0400

    create user's post api by post method

--- server/src/routes/post.js
+++ server/src/routes/post.js
@@ -1,3 +1,33 @@
+const PostModel = require("../models/PostModel");
+
 const router = require("express").Router();
+
+//CREATE POST
+router.post("/", async (req, res) => {
+  const newPost = await new PostModel({
+    ...req.body,
+  });
+  try {
+    await newPost.save();
+    res.status(200).send({ message: "Post created", newPost });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+//UPDATE FOR POST
+router.put("/:id", async (req, res) => {
+  try {
+    const newPost = await PostModel.findById({ _id: req.params.id });
+    if (newPost.userId === req.body.id) {
+      await newPost.updateOne({ $set: req.body });
+      res.status(200).send("Post updated succsefully");
+    } else {
+      res.status(403).send("You can't update other's post");
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 module.exports = router;
