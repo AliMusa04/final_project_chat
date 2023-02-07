7607d2acd7715c9b6727a0b02b953476cc4fc6b9
AliMusa04 <tu75nx176@code.edu.az>
2023-02-08 02:39:04 +0400

    delete post api

--- server/src/routes/post.js
+++ server/src/routes/post.js
@@ -1,4 +1,5 @@
 const PostModel = require("../models/PostModel");
+const { route } = require("./users");
 
 const router = require("express").Router();
 
@@ -16,18 +17,33 @@ router.post("/", async (req, res) => {
 });
 
 //UPDATE FOR POST
-// router.put("/:id", async (req, res) => {
-//   try {
-//     const newPost = await PostModel.findById({ _id: req.params.id });
-//     if (newPost.userId === req.body.id) {
-//       await newPost.updateOne({ $set: req.body });
-//       res.status(200).send("Post updated succsefully");
-//     } else {
-//       res.status(403).send("You can't update other's post");
-//     }
-//   } catch (err) {
-//     res.status(500).send(err);
-//   }
-// });
+router.put("/:id", async (req, res) => {
+  try {
+    const newPost = await PostModel.findById({ _id: req.params.id });
+    if (newPost.userId === req.body.id) {
+      await newPost.updateOne({ $set: req.body });
+      res.status(200).send("Post updated succsefully");
+    } else {
+      res.status(403).send("You can't update other's post");
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+//DELETE POST
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedPost = await PostModel.findById({ _id: req.params.id });
+    if (deletedPost.userId === req.body.id) {
+      await deletedPost.deleteOne();
+      res.status(200).send({ message: "This Post deleted", deletedPost });
+    } else {
+      res.status(400).send("You can't delete  other's post ");
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 module.exports = router;
